69a51264f1a4183b50426d597fc22aa0499759fd
Florian1215 <87897930+Florian1215@users.noreply.github.com>
2022-12-09 14:10:21 +0100

    Update minitalk.py

--- minitalk.py
+++ minitalk.py
@@ -1,11 +1,11 @@
 import os
-import re
 import sys
 import random
 import string
 
-pid = int(sys.argv[1]) if len(sys.argv) >= 2 else input('pid: ')
-length = int(sys.argv[2]) if len(sys.argv) >= 3 else 500
+pid = sys.argv[1] if len(sys.argv) >= 2 else input('pid: ')
+length = int(sys.argv[2]) if len(sys.argv) >= 3 and sys.argv[2].isdigit() else 500
+print_all_args = True if '-a' in sys.argv else False
 
 makefile_cmd = 'make'
 client_path = 'client'
@@ -16,7 +16,14 @@ if not os.path.exists(client_path):
 	print(f'don\'t find {client_path}')
 	exit()
 
-args = ''.join(random.choices(string.ascii_letters + string.digits + "!#$%*+,-./:;=?@^_~", k=length))
-print(f'ARGS = ...{args[-10:]}')
+if 'unicode' in sys.argv:
+	args = ''.join(random.choices('ரறலளழவஶஷஸஹఠడఢణతథదధనචඡජඣඤඥඦටඨའཡརལཤཥསཧཨ', k=length))
+else:
+	args = ''.join(random.choices(string.ascii_letters + string.digits + "!#$%*+,-./:;=?@^_~", k=length))
+
+if print_all_args:
+	print(f'ARGS = \'{args}\'')
+else:
+	print(f'ARGS = ...{args[-10:]}')
 print(os.popen(f'./{client_path} {pid} "{args}"').read().removesuffix('\n'))
 print('send !')
